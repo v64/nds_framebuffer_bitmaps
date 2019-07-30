04e7d0b807da10666ba05d961e795a9535e18662
Jahn Veach <j@hnvea.ch>
2019-07-30 00:03:45 -0400

    more

--- source/main.c
+++ source/main.c
@@ -26,11 +26,14 @@ int main(void) {
     int x, y;
     int gradx = 0;
     int grady = 0;
+    int time = 0;
     int step = 0;
 
     for (;;) {
         // Update state
-        step++;
+        time++;
+        if (time % 3 == 0) step++;
+
         scanKeys();
         uint32 keys = keysHeld();
         if (keys & KEY_UP) gradx++;
@@ -44,8 +47,8 @@ int main(void) {
         // Draw
         for (x = 0; x < 128; x++) {
             for (y = 0; y < 128; y++) {
-                DRAW128(videoMemoryMain, x, y, ARGB16(1, step+x % 32, gradx+y % 32, grady % 32));
-                DRAW128(videoMemorySub, x, y, ARGB16(1, gradx % 32, grady+x % 32, step+y % 32));
+                DRAW128(videoMemoryMain, x, y, ARGB16(1, step+x, gradx+x, gradx));
+                DRAW128(videoMemorySub, x, y, ARGB16(1, step+y, grady+x, grady));
             }
         }
     }
