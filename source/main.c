ca632d0070f7e223256b74ad9cd62f0c679fa283
Jahn Veach <j@hnvea.ch>
2019-07-29 22:20:21 -0400

    better colors

--- source/main.c
+++ source/main.c
@@ -29,39 +29,24 @@ int main(void) {
     int step = 0;
 
     for (;;) {
+        // Update state
         step++;
-
         scanKeys();
-
         uint32 keys = keysHeld();
+        if (keys & KEY_UP) gradx++;
+        if (keys & KEY_DOWN) gradx--;
+        if (keys & KEY_LEFT) grady++;
+        if (keys & KEY_RIGHT) grady--;
 
-        if (keys & KEY_UP) {
-            gradx++;
-            gradx = gradx % 256;
-        }
+        // Wait for vblank
+        swiWaitForVBlank();
 
-        if (keys & KEY_DOWN) {
-            gradx--;
-            gradx = gradx % 256;
-        }
-
-        if (keys & KEY_LEFT) {
-            grady++;
-            grady = grady % 256;
-        }
-
-        if (keys & KEY_RIGHT) {
-            grady--;
-            grady = grady % 256;
-        }
-
+        // Draw
         for (x = 0; x < 128; x++) {
             for (y = 0; y < 128; y++) {
-                DRAW128(videoMemoryMain, x, y, ARGB16(1, step % 32, gradx % 32, grady % 32));
-                DRAW128(videoMemorySub, x, y, ARGB16(1, gradx % 32, grady % 32, step % 32));
+                DRAW128(videoMemoryMain, x, y, ARGB16(1, step+x % 32, gradx+y % 32, grady % 32));
+                DRAW128(videoMemorySub, x, y, ARGB16(1, gradx % 32, grady+x % 32, step+y % 32));
             }
         }
-
-        swiWaitForVBlank();
     }
 }
